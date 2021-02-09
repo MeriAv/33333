6061962b1021874d151fe0350cc57d9b5e9a9a16
MeriAv <mari.avdeeva1407@gmal.com>
2021-02-09 16:12:28 +0300

    initial commit

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,40 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue("Челябинск");
+        form.$("[data-test-id=date] input").setValue("10.05.2020");
+        form.$("[data-test-id=name] input").setValue("Тестов Тест");
+        form.$("[data-test-id=phone] input").setValue("+785365214598");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(withText("Успешно!")).waitUntil(exist, 15000);
+    }
+
+    @Test
+    void shouldSubmigtRequestWithList() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue("Че");
+        $$(".menu-item__control").last().click();
+        form.$("[data-test-id=date] input").setValue("");
+        form.$(".input__icon").click();
+        $$("td").find(exactText("10")).click();
+        form.$("[data-test-id=name] input").setValue("Тестов Тест");
+        form.$("[data-test-id=phone] input").setValue("+785365214598");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(withText("Успешно!")).waitUntil(exist, 15000);
+    }
+
+
+}
